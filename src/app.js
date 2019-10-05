e900bb42f10d60b31b6afc358020205cd258db25
INTEL-PC\Intel <christian.vaccarino@hotmail.it>
2019-10-05 14:08:38 +0200

    Added Handlebars for dynamic page and removed HTML static page

--- src/app.js
+++ src/app.js
@@ -4,14 +4,36 @@ const path = require('path') //modulo core di Node per i path
 
 //console.log(__dirname) //stampa nome directory dove si trova questo file
 //console.log(__filename) //stampa tutto il percorso con il nome di questo file
-console.log(path.join(__dirname)) //comportamento uguale a sopra
+/*console.log(path.join(__dirname)) //comportamento uguale a sopra
 console.log(path.join(__dirname,'../..')) //per ogni coppia di .. torna indietro di una cartella
 console.log(path.join(__dirname,'../public')) //stampa dov'è publix
-
+*/
 const app = express()
 const publicDirectoryPath = path.join(__dirname,'../public')
 
+app.set('view engine','hbs')  //set da delle impostazioni ad Express, hbs è il modulo installato --> in questa riga abbiamo settato handlebars e ora possiamo creare template dinamici
 app.use(express.static(publicDirectoryPath))
+
+app.get('',(req,res) => {
+    res.render('index', { //renderizza una mia view, index deve coincidere con il nome della view (in questo caso index.hbs)
+        title: "Weather App",
+        name: "Christian"
+    }) 
+})
+
+app.get('/about',(req,res) => {
+    res.render('about', {
+        title: 'About Me',
+        name:'Christian'
+
+    })
+})
+
+app.get('/help',(req,res) => {
+    res.render('help', {
+        message: 'Help page for users'
+    })
+})
 
 /*app.get('',(req, res) => { //cosa restituisce
     //res.send('Hello Express!')
