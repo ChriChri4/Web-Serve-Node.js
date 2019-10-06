ec117af6997a99bbb3d6d1c3fd6d99da5e735e5d
INTEL-PC\Intel <christian.vaccarino@hotmail.it>
2019-10-06 16:46:33 +0200

    Added fetch function on JS client side

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,23 @@
 console.log('Client side javascript file is loaded!') //Questo messaggio dall opzione sviluppatori nel browser sarà visibile sulla console browser
+
+/*********************************************************************************** */
+/* Il codice qui è Javascript e non Node.JS! Questo è il client side Javascript code!*/
+/*********************************************************************************** */
+
+//ESEMPIO STANDARD
+/*fetch('http://puzzle.mead.io/puzzle').then((response) => { //url da cui vuoi prendere i dati --> fetch APi, prendi i dati dal fetch e THEN rispondi
+    response.json().then((data) => {
+        console.log(data) //sullo strumento sviluppatori in Chrome
+    })
+}) */ 
+
+fetch('http://localhost:3000/weather?address=torino').then((response) => {
+    response.json().then((data) => {
+        if(data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data.forecast)
+            console.log(data.location)
+        }
+    })
+})
